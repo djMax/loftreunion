8098640b4bf4b565651edeb0419b0ac5eede76c1
Max Metral <max@pyralis.com>
2020-09-10 13:05:54 -0400

    Updates

---
+++ precache-manifest.259f1561ef7561397a8127a7160fd45e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7aeba92ef3dfd760a82669d31c241f9",
+    "url": "/loftreunion/index.html"
+  },
+  {
+    "revision": "c58145c2f839dcfaecb1",
+    "url": "/loftreunion/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d4a1f9f0b867a98c7ac1",
+    "url": "/loftreunion/static/js/2.fd01935e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loftreunion/static/js/2.fd01935e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58145c2f839dcfaecb1",
+    "url": "/loftreunion/static/js/main.79f2741f.chunk.js"
+  },
+  {
+    "revision": "e8f8e29d168f479ab4e8",
+    "url": "/loftreunion/static/js/runtime-main.76212f47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/loftreunion/static/media/logo.5d5d9eef.svg"
+  }
+]);
