3869ba49012b0061e83473f76ea0f680d64eb472
Max Metral <max@pyralis.com>
2020-09-10 17:02:32 -0400

    Updates

---
+++ precache-manifest.424098ee5b378f889719aa3486a49762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ee71e6fcd3c16fd9bf7fb3e8a5b0bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e84c12bbdf258edbd9ce",
+    "url": "/static/css/main.ad563ea9.chunk.css"
+  },
+  {
+    "revision": "5722bf4379769b084ce6",
+    "url": "/static/js/2.cc5948ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc5948ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84c12bbdf258edbd9ce",
+    "url": "/static/js/main.20b59bb8.chunk.js"
+  },
+  {
+    "revision": "691223c9044f0c10d380",
+    "url": "/static/js/runtime-main.5d4b37be.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a2b1ed6b5d662b17bdc38071c3266a7.js"
+  "/precache-manifest.424098ee5b378f889719aa3486a49762.js"
 );
 
 self.addEventListener('message', (event) => {
