6c3316deda6d102d30a3584c3a489a498d189b9c
Max Metral <max@pyralis.com>
2020-09-10 13:49:21 -0400

    Updates

---
+++ precache-manifest.9a2b1ed6b5d662b17bdc38071c3266a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261a5afd06640c46641b0d7ad50f8597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a8300fcb6847ea03da6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d4a1f9f0b867a98c7ac1",
+    "url": "/static/js/2.fd01935e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd01935e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8300fcb6847ea03da6",
+    "url": "/static/js/main.056c4ced.chunk.js"
+  },
+  {
+    "revision": "691223c9044f0c10d380",
+    "url": "/static/js/runtime-main.5d4b37be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
