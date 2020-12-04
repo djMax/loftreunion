6286e0d4c220e13845952d2b009365f7e6bc38ae
Max Metral <max@pyralis.com>
2020-12-03 21:00:04 -0500

    Updates

--- precache-manifest.7ebd9a46a353d428cfcf8b823ee2dd0d.js
+++ precache-manifest.7ebd9a46a353d428cfcf8b823ee2dd0d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ab49fd67042bead5f886847038e6a08",
+    "revision": "5f89a5c8cdb80640a2d9fab03cf3acf6",
     "url": "/index.html"
   },
   {
-    "revision": "e84c12bbdf258edbd9ce",
-    "url": "/static/css/main.ad563ea9.chunk.css"
+    "revision": "1bc565e7d141bffbfa44",
+    "url": "/static/css/main.ee66fc93.chunk.css"
   },
   {
     "revision": "5722bf4379769b084ce6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.cc5948ba.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e84c12bbdf258edbd9ce",
-    "url": "/static/js/main.20b59bb8.chunk.js"
+    "revision": "1bc565e7d141bffbfa44",
+    "url": "/static/js/main.b6935a83.chunk.js"
   },
   {
     "revision": "691223c9044f0c10d380",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d28d16be88fdad00ab27e9f464df8a0c.js"
+  "/precache-manifest.7ebd9a46a353d428cfcf8b823ee2dd0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
